ea26f83f5b1d56a31dec8a16dceced65035efd7f
Jeremy Bezzant <jeremybezzant@Jeremys-MacBook-Pro.local>
2019-04-24 10:41:34 -0600

    finished first part

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className='splash'>
+          <nav className='nav-bar'>
+            <div className='nav-logo'>Start Bootstrap</div>
+            <div className='nav-buttons'>
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </div>
+              <div className='hamburger'><i class="fas fa-bars"></i></div>
+          </nav>
+          <div className='top-content'>
+            <h3>Welcome to Our Studio!</h3>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button>tell me more</button>
+          </div>
+        </div>
       </div>
     );
   }
